0e00bca55c45d6d57ddf0b2d3c9bd63f765df6e8
Clevoh <clevelihasi@gmail.com>
2023-07-20 03:10:43 -0700

    0 > 972?

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -11,11 +11,11 @@ int main(void)
 	int a, b, c;
 	int largest;
 
-	a = 97;
+	a = 972;
 	b = -98;
 	c = 0;
 
-	largest = largest_number (a, b, c);
+	largest = largest_number(a, b, c);
 
 	printf("%d is the largest number\n", largest);
 
