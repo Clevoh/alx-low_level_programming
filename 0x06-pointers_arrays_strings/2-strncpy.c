e740c5269bcf4bb2d93c57411c275cd7676a02d5
Clevoh <clevelihasi@gmail.com>
2023-07-26 10:10:15 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,13 +4,13 @@
  * @dest: string with concatination
  * @src: string to be concatinated
  * @n: size of the second string
- * Return: always 0.
+ * Return: always 0
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n; &&src[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		*(dest + i) = *(src + i);
 	for ( ; i < n; i++)
 		*(dest + i) = '\0';
