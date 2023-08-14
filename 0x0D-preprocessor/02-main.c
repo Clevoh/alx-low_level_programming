9a5842ee9e425c46c78588b214a7787cb457000d
Clevoh <clevelihasi@gmail.com>
2023-08-14 03:33:14 -0700

    2-main.c

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - prints the name of the file it was compiled from
+ *
+ * return: Always 0 (success)
+ */
 int main(void)
 {
-	printf("2-main.c: %s/n", __FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
