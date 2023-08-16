8f0884f63a0a209103dd374a4a67be1af84f1857
Clevoh <clevelihasi@gmail.com>
2023-08-16 03:12:36 -0700

    0. What's my name

--- 0x0F-function_pointers/main.h
+++ 0x0F-function_pointers/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#define <MAIN_H>
+#ifndef <MAIN.H>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
