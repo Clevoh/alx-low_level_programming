2520435492ae56426f3a72ca01a1e538d7b40158
Clevoh <clevelihasi@gmail.com>
2023-08-17 10:21:36 -0700

    2-print_strings.c

---
+++ 0x10-variadic_functions/2-main.c
@@ -0,0 +1,13 @@
+julien@ubuntu:~/0x0f. Variadic functions$ cat 2-main.c
+#include "variadic_functions.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_strings(", ", 2, "Jay", "Django");
+	return (0);
+}
