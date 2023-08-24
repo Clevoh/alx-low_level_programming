9f21b9094b5b8991f8c095358c30d9ccc7b4e0f0
Clevoh <clevelihasi@gmail.com>
2023-08-24 13:58:25 -0700

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * list_len -returns the number of elements in a linked list.
+ * @h: singly linked list.
+ * Return: number of elements in a linked list.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t i;
+
+	i = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
